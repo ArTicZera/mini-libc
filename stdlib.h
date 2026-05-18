0845aeaf21913cf6a36858bad782d5d765cf9a59
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-05-18 17:33:24 -0300

    Remove comments from stdlib.h

---
+++ stdlib.h
@@ -0,0 +1,16 @@
+#ifndef _STDLIB_H
+#define _STDLIB_H
+
+#include <stddef.h>
+
+void *malloc(size_t nbytes);
+void free(void *ap);
+void *calloc(size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
+
+int abs(int x);
+
+int atoi(const char *s);
+double atof(const char *s);
+
+#endif
