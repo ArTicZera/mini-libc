71605a2165eb2b7cd9b3732932f034e22edb6321
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-06-24 00:15:28 -0300

    Update comments in fcntl.h to English

--- fcntl.h
+++ fcntl.h
@@ -1,12 +1,12 @@
 #ifndef _FCNTL_H
 #define _FCNTL_H
 
-/* modos de abertura (flags para open) */
+/* Open flags */
 #define O_RDONLY 0
 #define O_WRONLY 1
 #define O_RDWR   2
 
-/* flags extras comuns (Doom pode ignorar a maioria) */
+/* Extra common flags */
 #define O_CREAT  0x40
 #define O_TRUNC  0x200
 #define O_APPEND 0x400
