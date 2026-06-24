c83b78e2199666ffa31335e527684e74e10f05b4
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-06-24 00:27:16 -0300

    Refactor memory management functions in stdlib.c

--- stdlib.c
+++ stdlib.c
@@ -20,101 +20,142 @@ union header
 typedef union header Header;
 
 static Header base;
-static Header *freep;
+static Header *freep = NULL;
 
-void free(void *ap) 
+void free(void *ap)
 {
-  if (ap == NULL)
-  {
-    return;
-  }
+    if (!ap)
+        return;
 
-  Header *bp, *p;
+    Header *bp = (Header *)ap - 1;
+    Header *p;
 
-  bp = (Header *)ap - 1;
-
-  for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
-    if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-      break;
-  if (bp + bp->s.size == p->s.ptr) {
-    bp->s.size += p->s.ptr->s.size;
-    bp->s.ptr = p->s.ptr->s.ptr;
-  } else
-    bp->s.ptr = p->s.ptr;
-  if (p + p->s.size == bp) {
-    p->s.size += bp->s.size;
-    p->s.ptr = bp->s.ptr;
-  } else
-    p->s.ptr = bp;
-  freep = p;
-}
-
-static Header *morecore(size_t nu) {
-  char *p;
-  Header *hp;
-
-  if (nu < 4096)
-    nu = 4096;
-  p = sbrk(nu * sizeof(Header));
-  if (p == (char *)-1)
-    return 0;
-  hp = (Header *)p;
-  hp->s.size = nu;
-  free((void *)(hp + 1));
-  return freep;
-}
-
-void *malloc(size_t nbytes) {
-  Header *p, *prevp;
-  size_t nunits;
-
-  nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
-  if ((prevp = freep) == 0) {
-    base.s.ptr = freep = prevp = &base;
-    base.s.size = 0;
-  }
-  for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr) {
-    if (p->s.size >= nunits) {
-      if (p->s.size == nunits)
-        prevp->s.ptr = p->s.ptr;
-      else {
-        p->s.size -= nunits;
-        p += p->s.size;
-        p->s.size = nunits;
-      }
-      freep = prevp;
-      return (void *)(p + 1);
+    if (freep == NULL) {
+        base.s.ptr = &base;
+        base.s.size = 0;
+        freep = &base;
     }
-    if (p == freep)
-      if ((p = morecore(nunits)) == 0)
-        return 0;
-  }
+
+    for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr) {
+        if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+            break;
+    }
+
+    if (bp + bp->s.size == p->s.ptr) {
+        bp->s.size += p->s.ptr->s.size;
+        bp->s.ptr = p->s.ptr->s.ptr;
+    } else {
+        bp->s.ptr = p->s.ptr;
+    }
+
+    if (p + p->s.size == bp) {
+        p->s.size += bp->s.size;
+        p->s.ptr = bp->s.ptr;
+    } else {
+        p->s.ptr = bp;
+    }
+
+    freep = p;
 }
 
-void *calloc(size_t nmemb, size_t size) {
-  void *data = malloc(nmemb * size);
-  if (data == NULL)
-    return NULL;
-  memset(data, 0, nmemb * size);
-  return data;
+#define NALLOC 1024
+
+static Header *morecore(size_t nu)
+{
+    char *cp;
+    Header *up;
+
+    if (nu < NALLOC)
+        nu = NALLOC;
+
+    cp = sbrk(nu * sizeof(Header));
+    if (cp == (char *)-1)
+        return NULL;
+
+    up = (Header *)cp;
+    up->s.size = nu;
+    
+    free((void *)(up + 1));
+
+    return freep;
 }
 
-void *realloc(void *ptr, size_t size) {
-  if (ptr == NULL)
-    return malloc(size);
-  if (size == 0) {
+void *malloc(size_t nbytes)
+{
+    Header *p, *prevp;
+    size_t nunits;
+
+    nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+
+    if (freep == NULL) {
+        base.s.ptr = &base;
+        base.s.size = 0;
+        freep = &base;
+    }
+
+    prevp = freep;
+
+    for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr) {
+        if (p->s.size >= nunits) {
+            if (p->s.size == nunits) {
+                prevp->s.ptr = p->s.ptr;
+            } else {
+                p->s.size -= nunits;
+                p += p->s.size;
+                p->s.size = nunits;
+            }
+
+            freep = prevp;
+            return (void *)(p + 1);
+        }
+
+        if (p == freep) {
+            p = morecore(nunits);
+            if (!p)
+                return NULL;
+        }
+    }
+}
+
+void *calloc(size_t nmemb, size_t size)
+{
+    if (nmemb != 0 && size > 0xFFFFFFFF / nmemb)
+        return NULL;
+
+    size_t total = nmemb * size;
+
+    void *data = malloc(total);
+    if (!data)
+        return NULL;
+
+    memset(data, 0, total);
+    return data;
+}
+
+void *realloc(void *ptr, size_t size)
+{
+    if (!ptr)
+        return malloc(size);
+
+    if (size == 0) {
+        free(ptr);
+        return NULL;
+    }
+
+    Header *h = (Header *)ptr - 1;
+
+    size_t old_size = h->s.size * sizeof(Header);
+
+    void *new_ptr = malloc(size);
+    if (!new_ptr)
+        return NULL;
+
+    size_t copy = old_size < size ? old_size : size;
+    memcpy(new_ptr, ptr, copy);
+
     free(ptr);
-    return NULL;
-  }
 
-  Header *header = (Header *)ptr - 1;
-  if (size == header->s.size)
-    return ptr; // no resize
-
-  void *new_data = malloc(size);
-  // Copy the minimum from the size before and requested size
-  memcpy(new_data, ptr, size < header->s.size ? size : header->s.size);
-  return new_data;
+    return new_ptr;
 }
 
 int abs(int x)
@@ -203,7 +244,10 @@ double atof(const char *s)
     return result * sign;
 }
 
-typedef unsigned long long uint64;
+double fabs(double x)
+{
+  return (x < 0.0) ? -x : x;
+}
 
 uint64 __divdi3(uint64 num, uint64 den)
 {
